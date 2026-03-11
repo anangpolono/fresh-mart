b5ed071e54d58f3f29bd22a81bb1600d7ab82e67
anangpolono <arissusanto.995@gmail.com>
2026-03-12 00:26:37 +0700

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <iframe
+      src="/freshmart.html"
+      style={{ width: "100%", height: "100vh", border: "none" }}
+      title="Fresh Mart Landing Page"
+    />
+  );
+}
